52fc3f8c2c6afe3f7318dcd23dc56298d35301c4
jiangxujin <jiangxujin@bytedance.com>
2025-12-23 15:00:35 +0800

    chore: update css config and dependencies

--- src/configs/css.ts
+++ src/configs/css.ts
@@ -1,12 +1,16 @@
 import css from '@eslint/css';
 import { defineConfig } from 'eslint/config';
 import prettier from 'eslint-plugin-prettier';
+import { tailwind4 } from 'tailwind-csstree';
 
 const cssConfig = defineConfig([
   {
     files: ['**/*.css'],
     plugins: { css, prettier },
     language: 'css/css',
+    languageOptions: {
+      customSyntax: tailwind4,
+    },
     rules: {
       'css/no-empty-blocks': 'error',
       'prettier/prettier': [
